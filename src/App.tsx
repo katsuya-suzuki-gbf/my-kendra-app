9b81861c562af418891868857092e773d48901b0
鈴木 勝也 <kt-suzuki@d-info.co.jp>
2025-09-22 09:43:44 +0900

    Retrieve対応２

--- src/App.tsx
+++ src/App.tsx
@@ -36,8 +36,8 @@ function App() {
   const [error, setError] = useState<string | null>(null);
 
   // C# APIのベースURL（ご自身の環境に合わせてください）
-  const API_BASE_URL = 'https://localhost:7283';
-  // const API_BASE_URL = 'https://cjdmuzpmsd.ap-northeast-1.awsapprunner.com';
+  // const API_BASE_URL = 'https://localhost:7283';
+  const API_BASE_URL = 'https://cjdmuzpmsd.ap-northeast-1.awsapprunner.com';
 
   const [showBackToTop, setShowBackToTop] = useState(false);
 
